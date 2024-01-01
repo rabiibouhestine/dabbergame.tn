6996b5cf29b4315ec6002f673a0b9ffd08ea7d5a
Rabii Bouhestine <rabiibouhestine@outlook.com>
2024-01-01 13:17:58 +0100

    edit game cover header padding

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -97,7 +97,7 @@
 	{#each games as game}
 		<div>
 			<div class="w-40 rounded-lg overflow-hidden">
-				<div class="bg-blue-600 px-3 py-2 text-white text-md font-bold">PS5</div>
+				<div class="bg-blue-600 px-3 py-1 text-white text-md font-bold">PS5</div>
 				<img
 					class="w-full h-48"
 					src="https://images.igdb.com/igdb/image/upload/t_cover_big/co7as5.jpg"
