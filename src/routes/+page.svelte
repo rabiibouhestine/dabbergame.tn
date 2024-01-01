797aafda40ab6d9483986388d4c1b6388271a3c8
Rabii Bouhestine <rabiibouhestine@outlook.com>
2024-01-01 15:06:21 +0100

    clean up home page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -93,8 +93,8 @@
 	];
 </script>
 
-<div class="flex flex-col max-w-5xl mx-auto my-10 px-4">
-	<div class="divider text-3xl font-bold my-10">Dernieres annonces</div>
+<div class="flex flex-col gap-y-10 max-w-5xl mx-auto my-10 px-4">
+	<div class="divider text-3xl font-bold">Dernieres annonces</div>
 	<div class="flex flex-wrap justify-center gap-8">
 		{#each games as game}
 			<div>
@@ -117,4 +117,5 @@
 			</div>
 		{/each}
 	</div>
+	<div class="self-center rounded-xl bg-base-300 px-4 py-2 text-xl">Voir toutes les annonces</div>
 </div>
