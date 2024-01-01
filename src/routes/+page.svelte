84aa23d6fe173a3722dbe4fb011795e1475fe62b
Rabii Bouhestine <rabiibouhestine@outlook.com>
2024-01-01 13:14:02 +0100

    add games to home page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,113 @@
-<h1 class="text-3xl font-bold underline">Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+	const games = [
+		{
+			name: 'REGICIDE SOLITAIRE',
+			href: '/regicide-solitaire',
+			time: 8,
+			weight: 1.94
+		},
+		{
+			name: 'SCOUNDREL',
+			href: '/scoundrel',
+			time: 5,
+			weight: 1.0
+		},
+		{
+			name: 'CLEAR THE DUNGEON',
+			href: '/clear-the-dungeon',
+			time: 3,
+			weight: 1.0
+		},
+		{
+			name: 'MERCHANT SOLITAIRE',
+			href: '/merchant-solitaire',
+			time: 5,
+			weight: 1.0
+		},
+		{
+			name: 'CARD CAPTURE',
+			href: '/card-capture',
+			time: 5,
+			weight: 1.4
+		},
+		{
+			name: 'AREA 52',
+			href: '/area-52',
+			time: 5,
+			weight: 1.3
+		},
+		{
+			name: 'REGICIDE SOLITAIRE',
+			href: '/regicide-solitaire',
+			time: 8,
+			weight: 1.94
+		},
+		{
+			name: 'SCOUNDREL',
+			href: '/scoundrel',
+			time: 5,
+			weight: 1.0
+		},
+		{
+			name: 'CLEAR THE DUNGEON',
+			href: '/clear-the-dungeon',
+			time: 3,
+			weight: 1.0
+		},
+		{
+			name: 'MERCHANT SOLITAIRE',
+			href: '/merchant-solitaire',
+			time: 5,
+			weight: 1.0
+		},
+		{
+			name: 'CARD CAPTURE',
+			href: '/card-capture',
+			time: 5,
+			weight: 1.4
+		},
+		{
+			name: 'AREA 52',
+			href: '/area-52',
+			time: 5,
+			weight: 1.3
+		},
+		{
+			name: 'MERCHANT SOLITAIRE',
+			href: '/merchant-solitaire',
+			time: 5,
+			weight: 1.0
+		},
+		{
+			name: 'CARD CAPTURE',
+			href: '/card-capture',
+			time: 5,
+			weight: 1.4
+		},
+		{
+			name: 'AREA 52',
+			href: '/area-52',
+			time: 5,
+			weight: 1.3
+		}
+	];
+</script>
+
+<div class="flex flex-wrap justify-center gap-2 sm:gap-12 max-w-5xl mx-auto my-10 px-4">
+	{#each games as game}
+		<div>
+			<div class="w-40 rounded-lg overflow-hidden">
+				<div class="bg-blue-600 px-3 py-2 text-white text-md font-bold">PS5</div>
+				<img
+					class="w-full h-48"
+					src="https://images.igdb.com/igdb/image/upload/t_cover_big/co7as5.jpg"
+					alt="cover"
+				/>
+			</div>
+			<div class="flex justify-between">
+				<div>Hello</div>
+				<div>200 DT</div>
+			</div>
+		</div>
+	{/each}
+</div>
