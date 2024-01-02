6b426c1ba23c597db94cfb7655ac2fa5545ee3b5
Rabii Bouhestine <rabiibouhestine@outlook.com>
2024-01-02 11:29:43 +0100

    build ui

--- src/routes/user/[id]/+page.svelte
+++ src/routes/user/[id]/+page.svelte
@@ -30,7 +30,7 @@
 	</div>
 </div>
 <div class="flex flex-col gap-8 w-full max-w-5xl mx-auto px-4 my-8">
-	<div class="divider divider-start text-3xl font-bold">Listings</div>
+	<div class="divider text-3xl font-bold">Listings</div>
 	<div class="flex flex-wrap justify-center gap-8">
 		{#each listings as listing}
 			<ListingCard />
