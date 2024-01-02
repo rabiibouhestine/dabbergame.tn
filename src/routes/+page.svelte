284bb3403252f2060d93256446ff7951eaa2056b
Rabii Bouhestine <rabiibouhestine@outlook.com>
2024-01-02 10:03:04 +0100

    add ui

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -95,72 +95,72 @@
 	];
 </script>
 
-<div class="flex flex-col gap-y-10 max-w-5xl mx-auto my-10 px-4">
-	<div class="h-64 flex flex-col justify-center gap-10 items-center">
-		<h2 class="text-center text-7xl font-bold">Dabber Games</h2>
-		<input type="text" placeholder="Type here" class="input input-bordered w-full" />
-	</div>
-	<div class="flex flex-col md:flex-row gap-4">
-		<div class="w-full flex flex-col gap-8 items-center bg-neutral p-4 rounded-3xl">
-			<h2 class="text-3xl font-bold">Popular Games</h2>
-			<div class="flex gap-6">
-				<a href="/">
-					<img
-						src="https://images.igdb.com/igdb/image/upload/t_cover_big/co7as5.jpg"
-						alt="game"
-						class="bg-red-600 rounded-xl w-36"
-					/>
-				</a>
-				<a href="/">
-					<img
-						src="https://images.igdb.com/igdb/image/upload/t_cover_big/co7ctx.jpg"
-						alt="game"
-						class="bg-red-600 rounded-xl w-36"
-					/>
-				</a>
-				<a href="/">
-					<img
-						src="https://images.igdb.com/igdb/image/upload/t_cover_big/co6lz0.jpg"
-						alt="game"
-						class="bg-red-600 rounded-xl w-36"
-					/>
-				</a>
-				<a href="/">
-					<img
-						src="https://images.igdb.com/igdb/image/upload/t_cover_big/co6jar.jpg"
-						alt="game"
-						class="bg-red-600 rounded-xl w-36"
-					/>
-				</a>
-			</div>
+<div class="h-64 flex flex-col justify-center gap-10 items-center">
+	<h2 class="text-center text-7xl font-bold">Dabber Games</h2>
+	<input type="text" placeholder="Type here" class="input input-bordered w-full" />
+</div>
+<div class="flex flex-col md:flex-row gap-4">
+	<div class="w-full flex flex-col gap-8 items-center bg-neutral p-4 rounded-3xl">
+		<h2 class="text-3xl font-bold">Popular Games</h2>
+		<div class="flex gap-6">
+			<a href="/">
+				<img
+					src="https://images.igdb.com/igdb/image/upload/t_cover_big/co7as5.jpg"
+					alt="game"
+					class="bg-red-600 rounded-xl w-36"
+				/>
+			</a>
+			<a href="/">
+				<img
+					src="https://images.igdb.com/igdb/image/upload/t_cover_big/co7ctx.jpg"
+					alt="game"
+					class="bg-red-600 rounded-xl w-36"
+				/>
+			</a>
+			<a href="/">
+				<img
+					src="https://images.igdb.com/igdb/image/upload/t_cover_big/co6lz0.jpg"
+					alt="game"
+					class="bg-red-600 rounded-xl w-36"
+				/>
+			</a>
+			<a href="/">
+				<img
+					src="https://images.igdb.com/igdb/image/upload/t_cover_big/co6jar.jpg"
+					alt="game"
+					class="bg-red-600 rounded-xl w-36"
+				/>
+			</a>
 		</div>
-		<div class="min-w-72 flex flex-col gap-8 bg-neutral p-4 rounded-3xl">
-			<h2 class="text-3xl text-center font-bold">Top Sellers</h2>
-			<div class="flex flex-col gap-3">
-				{#each [1, 2, 3, 4] as seller}
-					<div class="flex items-center justify-between">
-						<div class="flex gap-2 items-center">
-							<div class="avatar placeholder">
-								<div class="bg-base-200 text-neutral-content rounded-full w-10 h-10">
-									<span class="text-xs">Ls</span>
-								</div>
-							</div>
-							<div>
-								<h2>Foulen</h2>
-								<p class="text-xs">Mourouj</p>
+	</div>
+	<div class="min-w-72 flex flex-col gap-8 bg-neutral p-4 rounded-3xl">
+		<h2 class="text-3xl text-center font-bold">Top Sellers</h2>
+		<div class="flex flex-col gap-3">
+			{#each [1, 2, 3, 4] as seller}
+				<div class="flex items-center justify-between">
+					<div class="flex gap-2 items-center">
+						<div class="avatar placeholder">
+							<div class="bg-base-200 text-neutral-content rounded-full w-10 h-10">
+								<span class="text-xs">Ls</span>
 							</div>
 						</div>
-						<div class="text-2xl font-bold">4/5</div>
+						<div>
+							<h2>Foulen</h2>
+							<p class="text-xs">Mourouj</p>
+						</div>
 					</div>
-				{/each}
-			</div>
+					<div class="text-2xl font-bold">4/5</div>
+				</div>
+			{/each}
 		</div>
 	</div>
-	<div class="divider text-3xl font-bold">Latest Listings</div>
-	<div class="flex flex-wrap justify-center gap-8">
-		{#each games as game}
-			<ListingCard />
-		{/each}
-	</div>
-	<a href="/" class="self-center rounded-xl bg-base-300 px-4 py-2 text-xl"> See all listings </a>
 </div>
+<div class="divider text-3xl font-bold">Latest Listings</div>
+<div class="flex flex-wrap justify-center gap-8">
+	{#each games as game}
+		<ListingCard />
+	{/each}
+</div>
+<a href="/listings" class="self-center rounded-xl bg-base-300 px-4 py-2 text-xl">
+	See all listings
+</a>
---
+++ src/routes/listings/+page.svelte
@@ -0,0 +1,11 @@
+<script>
+	import ListingCard from '$lib/components/ListingCard.svelte';
+
+	let listings = Array.from({ length: 15 }, (_, index) => index + 1);
+</script>
+
+<div class="flex flex-wrap justify-center gap-8">
+	{#each listings as listing}
+		<ListingCard />
+	{/each}
+</div>
