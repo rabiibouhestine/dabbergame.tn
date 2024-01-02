41d224f8a0024503eeea7ab629d42c1b8ac4fdfa
Rabii Bouhestine <rabiibouhestine@outlook.com>
2024-01-02 11:24:10 +0100

    build ui

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -97,7 +97,7 @@
 
 <div class="flex flex-col gap-y-10 w-full max-w-5xl mx-auto my-10 px-4">
 	<div class="h-64 flex flex-col justify-center gap-10 items-center">
-		<h2 class="text-center text-7xl font-bold">Dabber Games</h2>
+		<h2 class="text-center text-7xl font-bold">Dabber Game</h2>
 		<input type="text" placeholder="Type here" class="input input-bordered w-full" />
 	</div>
 	<div class="flex flex-col md:flex-row gap-4">
