fcc6cbb44aea28bdbf42158e251a279f90403b50
Rabii Bouhestine <rabiibouhestine@outlook.com>
2024-01-02 09:53:03 +0100

    clean up home page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -166,13 +166,19 @@
 						alt="cover"
 					/>
 				</div>
-				<div class="flex justify-between py-2 items-center">
-					<div class="avatar placeholder">
-						<div class="bg-neutral text-neutral-content rounded-full w-8 h-8">
-							<span class="text-xs">Ls</span>
+				<div class="flex flex-col gap-2 py-2">
+					<div class="flex gap-2 items-center">
+						<div class="avatar placeholder">
+							<div class="bg-neutral text-neutral-content rounded-full w-10 h-10">
+								<span class="text-xs">Ls</span>
+							</div>
+						</div>
+						<div>
+							<h2>Foulen Falten</h2>
+							<p class="text-xs">Ben Arous</p>
 						</div>
 					</div>
-					<div class="text-2xl font-bold">200 DT</div>
+					<div class="text-2xl font-bold self-center">200 DT</div>
 				</div>
 			</div>
 		{/each}
