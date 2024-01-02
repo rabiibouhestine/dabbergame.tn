f2144d1e876227f1cabd22b0dddb44f5b8c88f6e
Rabii Bouhestine <rabiibouhestine@outlook.com>
2024-01-02 14:29:44 +0100

    build ui

---
+++ src/routes/listing/[id]/+page.svelte
@@ -0,0 +1,10 @@
+<script>
+	import GameCover from '$lib/components/GameCover.svelte';
+</script>
+
+<div class="grid grid-cols-4 gap-10 w-full max-w-5xl mx-auto my-10 px-4">
+	<div class="col-span-1">
+		<GameCover />
+	</div>
+	<div class="col-span-3">Info</div>
+</div>
