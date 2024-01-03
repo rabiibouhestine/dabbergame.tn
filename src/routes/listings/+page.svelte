375ea37acc7be9a3aadf43125efa6c14d50926cf
Rabii Bouhestine <rabiibouhestine@outlook.com>
2024-01-03 14:14:36 +0100

    build ui

--- src/routes/listings/+page.svelte
+++ src/routes/listings/+page.svelte
@@ -4,6 +4,66 @@
 	let listings = Array.from({ length: 15 }, (_, index) => index + 1);
 </script>
 
+<div class="bg-neutral">
+	<div class="grid grid-cols-1 md:grid-cols-4 gap-10 w-full max-w-5xl mx-auto px-4 py-6">
+		<div class="col-span-1 flex flex-col justify-center md:justify-start items-center gap-4">
+			<img
+				src="https://images.igdb.com/igdb/image/upload/t_cover_big/co7as5.jpg"
+				alt="game"
+				class="bg-red-600 rounded-xl w-full"
+			/>
+		</div>
+		<div class="col-span-3 flex flex-col gap-6">
+			<div class="divider divider-start text-5xl font-bold">The Finals</div>
+			<div class="flex flex-col gap-4">
+				<p>
+					Join The Finals, the world-famous, free-to-play, combat-centered game show! Fight
+					alongside your teammates in virtual arenas that you can alter, exploit, and even destroy.
+					Build your own playstyle in this first-person shooter to win escalating tournaments and
+					lasting fame. Can you reach The Finals?
+				</p>
+				<p>Genre: Shooter, Tactical</p>
+				<p>Platforms: PlayStation 5, PC (Microsoft Windows), Xbox Series X|S</p>
+			</div>
+			<div class="flex justify-between">
+				<div class="flex gap-3">
+					<a
+						href="/listings"
+						class="flex justify-center items-center bg-base-200 text-neutral-content rounded-full w-10 h-10"
+					>
+						<span class="text-md">S</span>
+					</a>
+					<a
+						href="/listings"
+						class="flex justify-center items-center bg-base-200 text-neutral-content rounded-full w-10 h-10"
+					>
+						<span class="text-md">W</span>
+					</a>
+					<a
+						href="/listings"
+						class="flex justify-center items-center bg-base-200 text-neutral-content rounded-full w-10 h-10"
+					>
+						<span class="text-md">T</span>
+					</a>
+				</div>
+				<div class="flex gap-3">
+					<a
+						href="/listings"
+						class="flex justify-center items-center bg-base-200 text-neutral-content rounded-full w-10 h-10"
+					>
+						<span class="text-md">89</span>
+					</a>
+					<a
+						href="/listings"
+						class="flex justify-center items-center bg-base-200 text-neutral-content rounded-full w-10 h-10"
+					>
+						<span class="text-md">87</span>
+					</a>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
 <div class="flex flex-col gap-y-10 w-full max-w-5xl mx-auto my-10 px-4">
 	<div class="flex flex-wrap justify-center gap-8">
 		{#each listings as listing}
