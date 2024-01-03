88c8603898f9c31cb5e929417568f1b51df2b52a
Rabii Bouhestine <rabiibouhestine@outlook.com>
2024-01-03 13:07:19 +0100

    build ui

--- src/lib/components/ListingCard.svelte
+++ src/lib/components/ListingCard.svelte
@@ -5,6 +5,9 @@
 <a href="/listing/1" class="min-w-40 flex-1">
 	<GameCover />
 	<div class="flex flex-col gap-2 py-2">
+		<div class="bg-neutral py-1 px-4 rounded-full w-full text-center text-2xl font-bold">
+			200 DT
+		</div>
 		<div class="flex gap-2 items-center">
 			<div class="avatar placeholder">
 				<div class="bg-neutral text-neutral-content rounded-full w-10 h-10">
@@ -16,6 +19,5 @@
 				<p class="text-xs">Ben Arous</p>
 			</div>
 		</div>
-		<div class="text-2xl font-bold self-center">200 DT</div>
 	</div>
 </a>
