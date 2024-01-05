debecf47db5b139465b50f951a7e53dfe0b5cd8c
Rabii Bouhestine <rabiibouhestine@outlook.com>
2024-01-05 11:30:57 +0100

    build ui

---
+++ src/lib/components/GameListingsHeader.svelte
@@ -0,0 +1,90 @@
+<script>
+	import Launch from '~icons/mdi/launch';
+	import Wikipedia from '~icons/mdi/wikipedia';
+	import Youtube from '~icons/mdi/youtube';
+	import Steam from '~icons/mdi/steam';
+	import Epicgames from '~icons/simple-icons/epicgames';
+
+	export let game;
+</script>
+
+<div class="bg-neutral rounded-3xl grid grid-cols-1 sm:grid-cols-4 gap-10 p-6 mb-8">
+	<div class="col-span-1 flex flex-col justify-center md:justify-start items-center gap-4">
+		<img
+			src="https://images.igdb.com/igdb/image/upload/t_cover_big/{game.cover.image_id}.jpg"
+			alt="game"
+			class="bg-red-600 rounded-xl w-full"
+		/>
+	</div>
+	<div class="col-span-3 flex flex-col gap-6">
+		<h2 class="text-5xl font-bold">
+			{game.name}
+		</h2>
+		<div class="flex flex-col gap-4">
+			<p class="line-clamp-4">{game.summary}</p>
+			<p>Genres: {game.genres.map((genre) => genre.name).join(', ')}</p>
+			<p>Platforms: {game.platforms.map((platform) => platform.name).join(', ')}</p>
+		</div>
+		<div class="flex justify-between">
+			{#if game.websites}
+				<div class="flex gap-3">
+					{#if game.websites.find((site) => site.category === 1)}
+						<a
+							href={game.websites.find((site) => site.category === 1).url}
+							target="_blank"
+							class="flex justify-center items-center bg-base-200 text-neutral-content rounded-full w-10 h-10"
+						>
+							<Launch />
+						</a>
+					{/if}
+					{#if game.websites.find((site) => site.category === 3)}
+						<a
+							href={game.websites.find((site) => site.category === 3).url}
+							target="_blank"
+							class="flex justify-center items-center bg-base-200 text-neutral-content rounded-full w-10 h-10"
+						>
+							<Wikipedia />
+						</a>
+					{/if}
+					{#if game.websites.find((site) => site.category === 9)}
+						<a
+							href={game.websites.find((site) => site.category === 9).url}
+							target="_blank"
+							class="flex justify-center items-center bg-base-200 text-neutral-content rounded-full w-10 h-10"
+						>
+							<Youtube />
+						</a>
+					{/if}
+					{#if game.websites.find((site) => site.category === 13)}
+						<a
+							href={game.websites.find((site) => site.category === 13).url}
+							target="_blank"
+							class="flex justify-center items-center bg-base-200 text-neutral-content rounded-full w-10 h-10"
+						>
+							<Steam />
+						</a>
+					{/if}
+					{#if game.websites.find((site) => site.category === 16)}
+						<a
+							href={game.websites.find((site) => site.category === 16).url}
+							target="_blank"
+							class="flex justify-center items-center bg-base-200 text-neutral-content rounded-full w-10 h-10"
+						>
+							<Epicgames />
+						</a>
+					{/if}
+				</div>
+			{/if}
+			<div class="flex gap-3">
+				{#if game.aggregated_rating}
+					<a
+						href="/listings"
+						class="flex justify-center items-center bg-base-200 text-neutral-content rounded-full px-4 h-10"
+					>
+						<span class="text-md">Rating: {Math.round(game.aggregated_rating)}</span>
+					</a>
+				{/if}
+			</div>
+		</div>
+	</div>
+</div>
--- src/routes/listings/[id]/+page.svelte
+++ src/routes/listings/[id]/+page.svelte
@@ -1,97 +1,14 @@
 <script>
+	import GameListingsHeader from '$lib/components/GameListingsHeader.svelte';
 	import ListingCard from '$lib/components/ListingCard.svelte';
-	import Launch from '~icons/mdi/launch';
-	import Wikipedia from '~icons/mdi/wikipedia';
-	import Youtube from '~icons/mdi/youtube';
-	import Steam from '~icons/mdi/steam';
-	import Epicgames from '~icons/simple-icons/epicgames';
 
 	export let data;
-	console.log(data);
+
 	let listings = Array.from({ length: 15 }, (_, index) => index + 1);
 </script>
 
 <div class="flex flex-col gap-y-8 w-full max-w-5xl mx-auto my-10 px-4">
-	<div class="bg-neutral rounded-3xl grid grid-cols-1 sm:grid-cols-4 gap-10 p-6 mb-8">
-		<div class="col-span-1 flex flex-col justify-center md:justify-start items-center gap-4">
-			<img
-				src="https://images.igdb.com/igdb/image/upload/t_cover_big/{data.game.cover.image_id}.jpg"
-				alt="game"
-				class="bg-red-600 rounded-xl w-full"
-			/>
-		</div>
-		<div class="col-span-3 flex flex-col gap-6">
-			<h2 class="text-5xl font-bold">
-				{data.game.name}
-			</h2>
-			<div class="flex flex-col gap-4">
-				<p class="line-clamp-4">{data.game.summary}</p>
-				<p>Genres: {data.game.genres.map((genre) => genre.name).join(', ')}</p>
-				<p>Platforms: {data.game.platforms.map((platform) => platform.name).join(', ')}</p>
-			</div>
-			<div class="flex justify-between">
-				{#if data.game.websites}
-					<div class="flex gap-3">
-						{#if data.game.websites.find((site) => site.category === 1)}
-							<a
-								href={data.game.websites.find((site) => site.category === 1).url}
-								target="_blank"
-								class="flex justify-center items-center bg-base-200 text-neutral-content rounded-full w-10 h-10"
-							>
-								<Launch />
-							</a>
-						{/if}
-						{#if data.game.websites.find((site) => site.category === 3)}
-							<a
-								href={data.game.websites.find((site) => site.category === 3).url}
-								target="_blank"
-								class="flex justify-center items-center bg-base-200 text-neutral-content rounded-full w-10 h-10"
-							>
-								<Wikipedia />
-							</a>
-						{/if}
-						{#if data.game.websites.find((site) => site.category === 9)}
-							<a
-								href={data.game.websites.find((site) => site.category === 9).url}
-								target="_blank"
-								class="flex justify-center items-center bg-base-200 text-neutral-content rounded-full w-10 h-10"
-							>
-								<Youtube />
-							</a>
-						{/if}
-						{#if data.game.websites.find((site) => site.category === 13)}
-							<a
-								href={data.game.websites.find((site) => site.category === 13).url}
-								target="_blank"
-								class="flex justify-center items-center bg-base-200 text-neutral-content rounded-full w-10 h-10"
-							>
-								<Steam />
-							</a>
-						{/if}
-						{#if data.game.websites.find((site) => site.category === 16)}
-							<a
-								href={data.game.websites.find((site) => site.category === 16).url}
-								target="_blank"
-								class="flex justify-center items-center bg-base-200 text-neutral-content rounded-full w-10 h-10"
-							>
-								<Epicgames />
-							</a>
-						{/if}
-					</div>
-				{/if}
-				<div class="flex gap-3">
-					{#if data.game.aggregated_rating}
-						<a
-							href="/listings"
-							class="flex justify-center items-center bg-base-200 text-neutral-content rounded-full px-4 h-10"
-						>
-							<span class="text-md">Rating: {Math.round(data.game.aggregated_rating)}</span>
-						</a>
-					{/if}
-				</div>
-			</div>
-		</div>
-	</div>
+	<GameListingsHeader game={data.game} />
 	<div class="flex justify-between">
 		<div class="flex items-center gap-3">
 			<select class="select select-bordered rounded-full">
