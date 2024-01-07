cd4aea02f776e195c7e628c3bd4fce7e826d0af7
Rabii Bouhestine <rabiibouhestine@outlook.com>
2024-01-07 15:28:44 +0100

    build ui

--- src/lib/components/ListingBanner.svelte
+++ src/lib/components/ListingBanner.svelte
@@ -8,7 +8,7 @@
 >
 	<UserInfo user={{ name: 'Foulen Ben Falten', state: 'Ben Arous', city: 'El Mourouj' }} />
 	<div class="flex flex-col sm:flex-row items-center gap-3">
-		<div class="badge badge-lg font-bold p-4">Playstation 5</div>
-		<div class="badge badge-lg font-bold p-4">200 DT</div>
+		<div class="badge badge-lg h-12 p-4 font-bold">Playstation 5</div>
+		<div class="badge badge-lg h-12 p-4 font-bold">200 DT</div>
 	</div>
 </a>
