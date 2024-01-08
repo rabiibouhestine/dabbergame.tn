9fafe2dd44cf936a1b524407a44a5b736b488fcd
Rabii Bouhestine <rabiibouhestine@outlook.com>
2024-01-08 12:44:39 +0100

    add login action

--- src/routes/(auth)/login/+page.server.js
+++ src/routes/(auth)/login/+page.server.js
@@ -1,5 +1,19 @@
+import { supabase } from "$lib/utils/supabaseClient";
+
 export const actions = {
-	default: async (event) => {
-		console.log("user login");
+	default: async ({ request }) => {
+		
+        const formData = await request.formData();
+		const email = formData.get('email');
+		const password = formData.get('password');
+
+        const { data, error } = await supabase.auth.signInWithPassword({
+            email: email,
+            password: password,
+        });
+
+        const { data: { user } } = await supabase.auth.getUser();
+
+        console.log(data.user);
 	}
 };
