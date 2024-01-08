e074463b766a06dcb4f9fb9f217ef347277badf4
Rabii Bouhestine <rabiibouhestine@outlook.com>
2024-01-08 11:40:46 +0100

    add register page form

---
+++ src/routes/(auth)/register/+page.svelte
@@ -0,0 +1,36 @@
+<form class="flex flex-col gap-4" method="POST">
+	<label class="form-control w-full max-w-xs">
+		<div class="label">
+			<span class="label-text">Email</span>
+		</div>
+		<input
+			name="email"
+			type="email"
+			placeholder="Type here"
+			class="input input-bordered w-full max-w-xs"
+		/>
+	</label>
+	<label class="form-control w-full max-w-xs">
+		<div class="label">
+			<span class="label-text">Password</span>
+		</div>
+		<input
+			name="password"
+			type="password"
+			placeholder="Type here"
+			class="input input-bordered w-full max-w-xs"
+		/>
+	</label>
+	<label class="form-control w-full max-w-xs">
+		<div class="label">
+			<span class="label-text">Confirm Password</span>
+		</div>
+		<input
+			name="password"
+			type="password"
+			placeholder="Type here"
+			class="input input-bordered w-full max-w-xs"
+		/>
+	</label>
+	<button class="btn btn-neutral">Signup</button>
+</form>
