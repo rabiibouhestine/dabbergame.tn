4e1a08adcc40b1c9ce9f56f26ba984f6ae2e0bae
Rabii Bouhestine <rabiibouhestine@outlook.com>
2024-01-08 14:47:18 +0100

    add auth modal boilerplate

--- src/lib/components/AuthModal.svelte
+++ src/lib/components/AuthModal.svelte
@@ -4,14 +4,45 @@
 	export function showModal() {
 		authModal.showModal();
 	}
+
+	function closeModal() {
+		authModal.close();
+	}
 </script>
 
 <dialog bind:this={authModal} class="modal">
 	<div class="modal-box">
-		<form method="dialog">
-			<button class="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">✕</button>
+		<form class="flex flex-col gap-4" method="POST">
+			<button
+				on:click={closeModal}
+				type="button"
+				class="btn btn-sm btn-circle btn-ghost absolute right-2 top-2"
+			>
+				✕
+			</button>
+			<label class="form-control w-full max-w-xs">
+				<div class="label">
+					<span class="label-text">Email</span>
+				</div>
+				<input
+					name="email"
+					type="email"
+					placeholder="Type here"
+					class="input input-bordered w-full max-w-xs"
+				/>
+			</label>
+			<label class="form-control w-full max-w-xs">
+				<div class="label">
+					<span class="label-text">Password</span>
+				</div>
+				<input
+					name="password"
+					type="password"
+					placeholder="Type here"
+					class="input input-bordered w-full max-w-xs"
+				/>
+			</label>
+			<button type="submit" class="btn btn-neutral">Log in</button>
 		</form>
-		<h3 class="font-bold text-lg">Hello!</h3>
-		<p class="py-4">Press ESC key or click on ✕ button to close</p>
 	</div>
 </dialog>
