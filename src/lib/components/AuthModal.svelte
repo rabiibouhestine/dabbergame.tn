70963a362cb44fa4762ec77e5d975f547ffb9b80
Rabii Bouhestine <rabiibouhestine@outlook.com>
2024-01-08 17:03:38 +0100

    add boilerplate to auth modal

--- src/lib/components/AuthModal.svelte
+++ src/lib/components/AuthModal.svelte
@@ -12,6 +12,8 @@
 
 <dialog bind:this={authModal} class="modal">
 	<div class="modal-box">
+		<h3 class="font-bold text-lg">Login to your account</h3>
+		<p class="py-4">You must be logged in to perform this action.</p>
 		<form class="flex flex-col gap-4" method="POST" action="?/register">
 			<button
 				on:click={closeModal}
@@ -27,7 +29,7 @@
 				<input
 					name="email"
 					type="email"
-					placeholder="Type here"
+					placeholder="Email Address"
 					class="input input-bordered w-full"
 				/>
 			</label>
@@ -38,11 +40,13 @@
 				<input
 					name="password"
 					type="password"
-					placeholder="Type here"
+					placeholder="Password"
 					class="input input-bordered w-full"
 				/>
 			</label>
+			<p class="py-4">Reset your password?</p>
 			<button class="btn btn-neutral">Signup</button>
 		</form>
+		<p class="py-4">Don't have an account? Sign up</p>
 	</div>
 </dialog>
