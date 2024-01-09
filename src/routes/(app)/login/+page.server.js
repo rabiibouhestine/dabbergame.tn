f4cbda52b2e9ae9b42130faee83f22c80507757a
Rabii Bouhestine <rabiibouhestine@outlook.com>
2024-01-09 22:29:45 +0100

    improve forms

--- src/routes/(app)/login/+page.server.js
+++ src/routes/(app)/login/+page.server.js
@@ -2,44 +2,44 @@ import { AuthApiError } from "@supabase/supabase-js";
 import { fail, redirect } from "@sveltejs/kit";
 
 export const actions = {
-  default: async (event) => {
-    const { request, url, locals: { supabase } } = event;
-		
-    const formData = await request.formData();
-    const email = formData.get('email');
-    const password = formData.get('password');
+    default: async (event) => {
+        const { request, url, locals: { supabase } } = event;
+            
+        const formData = await request.formData();
+        const email = formData.get('email');
+        const password = formData.get('password');
 
-    if (!email) {
-        return fail(400, {
-            email,
-            error: "Missing Email"
-        });
-    }
-
-    if (!password) {
-        return fail(400, {
-            email,
-            error: "Missing Password"
-        });
-    }
-
-    const { data, error } = await supabase.auth.signInWithPassword({
-        email: email,
-        password: password,
-    });
-
-    if (error) {
-        if (error instanceof AuthApiError && error.status === 400) {
+        if (!email) {
             return fail(400, {
                 email,
-                error: "Invalid credentials",
+                error: "Missing Email"
+            });
+        }
+
+        if (!password) {
+            return fail(400, {
+                email,
+                error: "Missing Password"
+            });
+        }
+
+        const { data, error } = await supabase.auth.signInWithPassword({
+            email: email,
+            password: password,
+        });
+
+        if (error) {
+            if (error instanceof AuthApiError && error.status === 400) {
+                return fail(400, {
+                    email,
+                    error: "Invalid credentials",
+                })
+            }
+            return fail(500, {
+                message: "Server error. Try again later.",
             })
         }
-        return fail(500, {
-            message: "Server error. Try again later.",
-        })
-    }
 
-    throw redirect(303, "/");
-  }
+        throw redirect(303, "/");
+    }
 };
