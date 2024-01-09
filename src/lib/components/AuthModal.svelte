3cf66e3bc2f3ef120f03119ecf0a41280e9872b2
Rabii Bouhestine <rabiibouhestine@outlook.com>
2024-01-09 12:34:16 +0100

    add profile data to signup form modal

--- src/lib/components/AuthModal.svelte
+++ src/lib/components/AuthModal.svelte
@@ -95,6 +95,75 @@
 					class="input input-bordered w-full"
 				/>
 			</label>
+			<label class="form-control w-full">
+				<div class="label">
+					<span class="label-text">Confirm Password</span>
+				</div>
+				<input
+					name="confirm password"
+					type="password"
+					placeholder="Password"
+					class="input input-bordered w-full"
+				/>
+			</label>
+			<div class="flex gap-3">
+				<label class="form-control w-full">
+					<div class="label">
+						<span class="label-text">First Name</span>
+					</div>
+					<input
+						name="first name"
+						type="text"
+						placeholder="First Name"
+						class="input input-bordered w-full"
+					/>
+				</label>
+				<label class="form-control w-full">
+					<div class="label">
+						<span class="label-text">Last Name</span>
+					</div>
+					<input
+						name="last name"
+						type="text"
+						placeholder="Last Name"
+						class="input input-bordered w-full"
+					/>
+				</label>
+			</div>
+			<div class="flex gap-3">
+				<label class="form-control w-full">
+					<div class="label">
+						<span class="label-text">State</span>
+					</div>
+					<select class="select select-bordered">
+						<option disabled selected>State</option>
+						<option>Star Wars</option>
+						<option>Harry Potter</option>
+						<option>Lord of the Rings</option>
+						<option>Planet of the Apes</option>
+						<option>Star Trek</option>
+					</select>
+				</label>
+				<label class="form-control w-full">
+					<div class="label">
+						<span class="label-text">City</span>
+					</div>
+					<select class="select select-bordered">
+						<option disabled selected>City</option>
+						<option>Star Wars</option>
+						<option>Harry Potter</option>
+						<option>Lord of the Rings</option>
+						<option>Planet of the Apes</option>
+						<option>Star Trek</option>
+					</select>
+				</label>
+			</div>
+			<label class="form-control w-full">
+				<div class="label">
+					<span class="label-text">Phone</span>
+				</div>
+				<input name="phone" type="text" placeholder="Phone" class="input input-bordered w-full" />
+			</label>
 			<button class="btn btn-neutral">Signup</button>
 		</form>
 		<span>
