70dd586a9aa7dd03dbf792c535095935ac129a60
Rabii Bouhestine <rabiibouhestine@outlook.com>
2024-01-10 18:08:07 +0100

    improve code

--- src/routes/(app)/post-listing/+page.svelte
+++ src/routes/(app)/post-listing/+page.svelte
@@ -2,6 +2,14 @@
 	import SearchBar from '$lib/components/SearchBar.svelte';
 	import { getGameCover } from '$lib/utils/igdbUtils';
 
+	let gameSelected = false;
+	let game_id = '';
+
+	function handleGameSelection(event) {
+		gameSelected = true;
+		game_id = event.detail;
+	}
+
 	const popular = [
 		{
 			image_id: 'co7as5'
@@ -18,16 +26,21 @@
 	];
 </script>
 
-<div class="flex flex-col gap-8 max-w-lg mx-auto">
-	<SearchBar />
-	<h2 class="text-4xl font-bold">Suggestions</h2>
-	<div class="w-full flex flex-col gap-8 rounded-container">
-		<div class="h-full flex flex-wrap justify-center items-center gap-6">
-			{#each popular as game}
-				<a href="/listings/1">
-					<img src={getGameCover(game.image_id)} alt="game" class="rounded-xl w-32" />
-				</a>
-			{/each}
+<div class="flex flex-col gap-8 max-w-2xl mx-auto">
+	<div class="divider divider-start text-4xl font-bold">Create Listing</div>
+	<SearchBar on:click={handleGameSelection} />
+	{#if !gameSelected}
+		<div class="w-full flex flex-col gap-8 rounded-container">
+			<div class="divider divider-start text-4xl font-bold">Popular Games</div>
+			<div class="h-full flex flex-wrap justify-center items-center gap-6">
+				{#each popular as game}
+					<a href="/listings/1">
+						<img src={getGameCover(game.image_id)} alt="game" class="rounded-xl w-32" />
+					</a>
+				{/each}
+			</div>
 		</div>
-	</div>
+	{:else}
+		<h2>{game_id}</h2>
+	{/if}
 </div>
