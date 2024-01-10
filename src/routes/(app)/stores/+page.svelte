d2ee490233f4a67efd544710b37391ff134a2c82
Rabii Bouhestine <rabiibouhestine@outlook.com>
2024-01-10 14:43:25 +0100

    improve crud

--- src/routes/(app)/stores/+page.svelte
+++ src/routes/(app)/stores/+page.svelte
@@ -6,7 +6,7 @@
 	<div class="divider divider-start text-4xl font-bold">Stores</div>
 	<div class="flex flex-col gap-y-8">
 		{#each [1, 2, 3, 4, 5, 6] as listing}
-			<UserCard />
+			<UserCard profile={{}} />
 		{/each}
 	</div>
 </div>
