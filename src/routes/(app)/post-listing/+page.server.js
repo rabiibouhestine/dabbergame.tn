853ba5348241c02b24e48574a11856ce55651953
Rabii Bouhestine <rabiibouhestine@outlook.com>
2024-01-11 13:46:57 +0100

    improve code

--- src/routes/(app)/post-listing/+page.server.js
+++ src/routes/(app)/post-listing/+page.server.js
@@ -1,10 +1,12 @@
 import { AuthApiError } from "@supabase/supabase-js";
 import { fail, redirect } from "@sveltejs/kit";
+import { IGDB_CLIENT_ID, IGDB_TOKEN } from '$env/static/private';
 
 export const actions = {
 	default: async (event) => {
 		const { request, url, locals: { supabase } } = event;
 
+        // Get Listing Form Data
         const formData = await request.formData();
         const game_id = formData.get('game_id');
         const platform = formData.get('platform');
@@ -14,8 +16,24 @@ export const actions = {
         const store_link = formData.get('store_link');
         const trade = formData.get('trade');
 
+        // Get Game Info from IGDB API
+        const apiUrl = "https://api.igdb.com/v4/games";
+        const headers = {
+            'Accept': 'application/json',
+            'Authorization': 'Bearer ' + IGDB_TOKEN,
+            'Client-ID': IGDB_CLIENT_ID
+        };
+        const body = "fields id,cover.image_id,name,first_release_date,summary,genres.name,platforms.name,aggregated_rating,websites.category,websites.url;\nwhere id = " + game_id + ";"
+        const response = await fetch(apiUrl, {
+            method: 'POST',
+            headers: headers,
+            body: body
+        })
+        const data = await response.json();
+        const game = data[0];
 
-        console.log(formData.get('trade'));
+        // Save Listing in the database
+        console.log(game);
 
 	}
 };
