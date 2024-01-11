28a81cdf54c2c68c9fbbd8393e307966ddd5eacb
Rabii Bouhestine <rabiibouhestine@outlook.com>
2024-01-11 16:15:54 +0100

    improve code

--- src/lib/components/ListingBanner.svelte
+++ src/lib/components/ListingBanner.svelte
@@ -6,7 +6,7 @@
 	href="/listing/1"
 	class="flex flex-col sm:flex-row justify-between items-center gap-6 rounded-container"
 >
-	<UserInfo user={{ name: 'Foulen Ben Falten', state: 'Ben Arous', city: 'El Mourouj' }} />
+	<UserInfo />
 	<div class="flex flex-col sm:flex-row items-center gap-3">
 		<div class="badge badge-lg h-12 p-4 font-bold">Playstation 5</div>
 		<div class="badge badge-lg h-12 p-4 font-bold">200 DT</div>
