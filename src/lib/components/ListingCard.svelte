d04f318e5e3a713a9310354b154f0f2b4e5a2b36
Rabii Bouhestine <rabiibouhestine@outlook.com>
2024-01-12 22:19:22 +0100

    cleanup

--- src/lib/components/ListingCard.svelte
+++ src/lib/components/ListingCard.svelte
@@ -19,7 +19,7 @@
 		/>
 	</div>
 	{#if showPrice}
-		<div class="bg-neutral py-1 px-4 rounded-full w-full text-center text-2xl font-bold">
+		<div class="badge badge-outline py-4 px-4 rounded-full w-full text-center text-2xl font-bold">
 			{listing.listing_price} DT
 		</div>
 	{/if}
