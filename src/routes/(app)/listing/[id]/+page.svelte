b436a2c4fce305093953afd2b7fda56571e0fb6f
Rabii Bouhestine <rabiibouhestine@outlook.com>
2024-01-12 22:17:00 +0100

    cleanup

--- src/routes/(app)/listing/[id]/+page.svelte
+++ src/routes/(app)/listing/[id]/+page.svelte
@@ -31,8 +31,14 @@
 	<div class="min-w-52 flex flex-col justify-center items-center gap-4">
 		<ListingCard showPrice={false} listing={data.listing} />
 		<div class="w-full flex flex-col gap-3">
-			<span class="bg-base-300 rounded-full px-4 py-2 text-lg text-center font-bold">Phone</span>
-			<span class="bg-base-300 rounded-full px-4 py-2 text-lg text-center font-bold">Buy Now</span>
+			<span class="btn btn-outline btn-accent rounded-full px-4 py-2 text-lg text-center font-bold">
+				Phone
+			</span>
+			<span
+				class="btn btn-outline btn-primary rounded-full px-4 py-2 text-lg text-center font-bold"
+			>
+				Buy Now
+			</span>
 		</div>
 	</div>
 	<div class="flex flex-col gap-6">
