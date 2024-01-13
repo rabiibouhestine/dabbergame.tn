a14cc15350368148e64280f9d17702f559810be9
Rabii Bouhestine <rabiibouhestine@outlook.com>
2024-01-13 15:19:26 +0100

    cleanup

--- src/lib/components/UserInfo.svelte
+++ src/lib/components/UserInfo.svelte
@@ -3,11 +3,11 @@
 </script>
 
 <div class="flex gap-2 items-center">
-	<img
-		src={'https://gravatar.com/avatar/' + 'hashedEmail'}
-		alt="profile"
-		class="rounded-full w-10 h-10"
-	/>
+	<div class="avatar placeholder">
+		<div class="bg-neutral text-neutral-content rounded-full w-10">
+			<span class="font-bold">{user?.first_name[0] + user?.last_name[0]}</span>
+		</div>
+	</div>
 	<div>
 		<h2 class="line-clamp-1 font-bold">{user?.first_name + ' ' + user?.last_name}</h2>
 		<p class="line-clamp-1 text-xs">{user?.cities.state + ', ' + user?.cities.city}</p>
