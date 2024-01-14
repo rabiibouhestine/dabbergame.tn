730ef7ed9d0d00376aabb278efbd3649bf52d7e0
Rabii Bouhestine <rabiibouhestine@outlook.com>
2024-01-14 18:50:28 +0100

    cleanup

--- src/routes/(app)/+page.svelte
+++ src/routes/(app)/+page.svelte
@@ -57,7 +57,7 @@
 		<div class="w-full flex flex-col gap-6 rounded-container justify-between">
 			<div>
 				<h2 class="text-4xl font-bold">Popular Games</h2>
-				<p class="text-neutral-content">Based on the number of listings for each game.</p>
+				<p class="text-neutral-content">Based on the number of game listings.</p>
 			</div>
 			<div class="grid grid-cols-2 sm:grid-cols-4 gap-4">
 				{#each popular as game}
