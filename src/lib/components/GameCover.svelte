c4f9f19f6e5387f04f6a81b71dc2f0767d143b85
Rabii Bouhestine <rabiibouhestine@outlook.com>
2024-01-14 23:12:25 +0100

    cleanup

--- src/lib/components/GameCover.svelte
+++ src/lib/components/GameCover.svelte
@@ -9,7 +9,7 @@
 	export let platform;
 	export let coverImageId;
 
-	$: Test = getLogo(platform);
+	$: Logo = getLogo(platform);
 
 	function getLogo(platform) {
 		switch (platform) {
@@ -19,19 +19,40 @@
 				return Playstation;
 			case 'Xbox Series X|S':
 				return Xbox;
+			case 'Xbox One':
+				return Xbox;
 			case 'Nintendo Switch':
 				return NintendoSwitch;
 			default:
 				return Windows;
 		}
 	}
+
+	function getBG(platform) {
+		switch (platform) {
+			case 'PlayStation 4':
+				return 'bg-playstation';
+			case 'PlayStation 5':
+				return 'bg-playstation';
+			case 'Xbox Series X|S':
+				return 'bg-xbox';
+			case 'Xbox One':
+				return 'bg-xbox';
+			case 'Nintendo Switch':
+				return 'bg-nintendo';
+			default:
+				return 'bg-pc';
+		}
+	}
 </script>
 
 <div class="aspect-[5/7] rounded-lg overflow-hidden">
 	<div
-		class="h-[15%] flex gap-1 items-center bg-playstation px-3 py-1 text-sm text-slate-200 font-bold"
+		class="h-[15%] flex gap-1 items-center {getBG(
+			platform
+		)} px-3 py-1 text-sm text-slate-200 font-bold"
 	>
-		<Test />
+		<Logo />
 		<p class="line-clamp-1">{platform}</p>
 	</div>
 	<img class="w-full" src={getGameCover(coverImageId)} alt="cover" />
