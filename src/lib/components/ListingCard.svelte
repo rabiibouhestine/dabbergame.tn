77631297cbfecd48555ac0d0b51593d70060c227
Rabii Bouhestine <rabiibouhestine@outlook.com>
2024-01-14 18:10:15 +0100

    cleanup

--- src/lib/components/ListingCard.svelte
+++ src/lib/components/ListingCard.svelte
@@ -8,14 +8,13 @@
 </script>
 
 <div class="flex flex-col gap-3">
-	<a
-		href="/listing/{listing.id}"
-		class="flex flex-col gap-3 group transition ease-in-out hover:scale-110"
-	>
-		<GameCover platform={listing.listing_platform} coverImageId={listing.game_cover} />
+	<a href="/listing/{listing.id}" class="flex flex-col gap-3 group">
+		<div class="transition ease-in-out group-hover:scale-110">
+			<GameCover platform={listing.listing_platform} coverImageId={listing.game_cover} />
+		</div>
 		{#if showPrice}
 			<div
-				class="badge badge-neutral p-4 w-full text-2xl font-bold transition ease-in-out group-hover:badge-secondary group-hover:-translate-y-8"
+				class="badge badge-neutral p-4 w-full text-2xl font-bold transition ease-in-out group-hover:badge-secondary group-hover:-translate-y-4"
 			>
 				{listing.listing_price} DT
 			</div>
