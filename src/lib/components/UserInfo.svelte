0652c3767848c137ebb3cbbdef33d08b98ddc32f
Rabii Bouhestine <rabiibouhestine@outlook.com>
2024-01-15 11:07:44 +0100

    cleanup

--- src/lib/components/UserInfo.svelte
+++ src/lib/components/UserInfo.svelte
@@ -4,12 +4,12 @@
 
 <div class="flex gap-2 items-center">
 	<div class="avatar placeholder">
-		<div class="bg-neutral text-neutral-content rounded-full w-10">
-			<span class="font-bold">{user?.first_name[0] + user?.last_name[0]}</span>
+		<div class="bg-neutral text-neutral-content rounded-full w-8">
+			<span class="font-bold text-sm">{user?.first_name[0] + user?.last_name[0]}</span>
 		</div>
 	</div>
 	<div>
-		<h2 class="line-clamp-1 font-bold">{user?.first_name + ' ' + user?.last_name}</h2>
+		<h2 class="line-clamp-1 font-bold text-sm">{user?.first_name + ' ' + user?.last_name}</h2>
 		<p class="line-clamp-1 text-xs text-neutral-content">
 			{user?.cities.state + ', ' + user?.cities.city}
 		</p>
