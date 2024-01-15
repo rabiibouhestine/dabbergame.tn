6716c4659d3d5023e53c345d31eed46a87636e1f
Rabii Bouhestine <rabiibouhestine@outlook.com>
2024-01-15 11:04:53 +0100

    cleanup

--- src/routes/(app)/+page.server.js
+++ src/routes/(app)/+page.server.js
@@ -5,6 +5,7 @@ export async function load({ locals }) {
     const listingsQuery = await supabase
     .from('listings')
     .select(`*, profiles (id, first_name, last_name, cities (id, state, city))`)
+    .limit(18)
 
     return {
         listings: listingsQuery.data
