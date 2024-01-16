0a4b0f6a4c2863ab6d1389409959be30e94f7cf6
Rabii Bouhestine <rabiibouhestine@outlook.com>
2024-01-16 22:10:19 +0100

    cleanup

--- src/lib/components/GameCover.svelte
+++ src/lib/components/GameCover.svelte
@@ -54,7 +54,7 @@
 	}
 </script>
 
-<div class="aspect-[5/7] rounded-lg overflow-hidden bg-neutral">
+<div class="aspect-[5/7] rounded-lg overflow-hidden bg-base-100">
 	<div
 		class="h-[15%] flex gap-1 items-center {BackgoundColor} px-3 py-1 text-sm text-slate-200 font-bold"
 	>
