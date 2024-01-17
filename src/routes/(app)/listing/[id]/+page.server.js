bb7b8a88b56d0bfa8ad64ad5ddb10138c898800e
Rabii Bouhestine <rabiibouhestine@outlook.com>
2024-01-17 20:02:53 +0100

    cleanup

--- src/routes/(app)/listing/[id]/+page.server.js
+++ src/routes/(app)/listing/[id]/+page.server.js
@@ -7,8 +7,9 @@ export async function load({ params, locals }) {
     .from('listings')
     .select(`*, profiles (id, first_name, last_name, cities (id, state, city))`)
     .eq('id', id)
+    .single()
 
     return {
-        listing: listingQuery.data[0]
+        listing: listingQuery.data
     };
 }
