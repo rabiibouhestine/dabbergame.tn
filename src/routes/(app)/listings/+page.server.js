633b8380064b54ebb59250f8b91280cd45e5186e
Rabii Bouhestine <rabiibouhestine@outlook.com>
2024-01-17 13:09:31 +0100

    cleanup

--- src/routes/(app)/listings/+page.server.js
+++ src/routes/(app)/listings/+page.server.js
@@ -2,7 +2,7 @@
 export async function load({ url, locals }) {
 
     const maxPrice = Number(url.searchParams.get('maxPrice')) || 300;
-    const cityId = Number(url.searchParams.get('cityId')) || "18";
+    const cityId = Number(url.searchParams.get('cityId'));
 
     const page = Number(url.searchParams.get('page')) || 1;
     const limit = 6;
@@ -10,13 +10,18 @@ export async function load({ url, locals }) {
 
     const supabase = locals.supabase;
 
-    const listingsQuery = await supabase
+    let listingsQuery = supabase
     .from('listings')
     .select('id, game_cover, listing_platform_family, listing_platform, listing_price, profiles (id, first_name, last_name, cities (id, state, city))', { count: 'exact' })
-    .eq('city_id', cityId)
     .lt('listing_price', maxPrice)
     .order('listing_price', { ascending: false })
-    .range(skip, skip + limit - 1)
+    .range(skip, skip + limit - 1);
+  
+    if (cityId) {
+        listingsQuery = listingsQuery.eq('city_id', cityId);
+    }
+  
+    listingsQuery = await listingsQuery;
 
     const citiesQuery = await supabase.from("cities").select();
 
