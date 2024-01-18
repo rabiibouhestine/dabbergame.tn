5131f98d3ab85345141dda617a21a6cb905188de
Rabii Bouhestine <rabiibouhestine@outlook.com>
2024-01-18 12:39:01 +0100

    cleanup

--- src/lib/components/StoreCard.svelte
+++ src/lib/components/StoreCard.svelte
@@ -5,19 +5,28 @@
 	import Store from '~icons/mdi/store';
 
 	export let profile;
+
+	export let firstName = 'John';
+	export let lastName = 'Doe';
+	export let state = 'State';
+	export let city = 'City';
+	export let phone = '';
+	export let facebookLink = '';
+	export let instagramLink = '';
+	export let storeLink = '';
 </script>
 
 <div class="rounded-container flex flex-col justify-between items-center gap-8">
 	<div class="flex flex-col gap-4 items-center">
 		<div class="avatar placeholder">
 			<div class="bg-neutral text-neutral-content rounded-full w-24">
-				<span class="font-bold">{profile?.first_name[0] + profile?.last_name[0]}</span>
+				<span class="font-bold text-3xl">{firstName[0] + lastName[0]}</span>
 			</div>
 		</div>
 		<div class="text-center">
-			<h2 class="text-3xl font-bold">{profile?.first_name + ' ' + profile?.last_name}</h2>
+			<h2 class="text-3xl font-bold">{firstName + ' ' + lastName}</h2>
 			<p class="text-lg text-neutral-content">
-				{profile?.cities.state + ', ' + profile?.cities.city}
+				{state + ', ' + city}
 			</p>
 		</div>
 	</div>
