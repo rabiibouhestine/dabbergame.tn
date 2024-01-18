06560ae47c8b3c867b358b4beff0376ab6c10616
Rabii Bouhestine <rabiibouhestine@outlook.com>
2024-01-18 10:34:55 +0100

    cleanup

--- src/routes/(app)/listings/+page.server.js
+++ src/routes/(app)/listings/+page.server.js
@@ -2,6 +2,7 @@
 export async function load({ url, locals }) {
 
     const maxPrice = Number(url.searchParams.get('maxPrice')) || 300;
+    const state = url.searchParams.get('state') || 'All States';
     const city = url.searchParams.get('city') || 'All Cities';
 
     const page = Number(url.searchParams.get('page')) || 1;
@@ -15,6 +16,10 @@ export async function load({ url, locals }) {
     let listingsViewQuery = supabase
     .from('listings_full')
     .select('*', { count: 'exact' })
+
+    if (state && state !== 'All States') {
+        listingsViewQuery = listingsViewQuery.eq('state', state);
+    }
 
     if (city && city !== 'All Cities') {
         listingsViewQuery = listingsViewQuery.eq('city', city);
