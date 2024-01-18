7d1c845d0b8f275ce58a4436cbf66aeb36ad9358
Rabii Bouhestine <rabiibouhestine@outlook.com>
2024-01-18 13:25:14 +0100

    cleanup

--- src/routes/(app)/stores/+page.svelte
+++ src/routes/(app)/stores/+page.svelte
@@ -5,18 +5,18 @@
 	import StoreIcon from '~icons/mdi/store';
 </script>
 
-<div class="flex flex-col gap-y-10">
+<div class="flex flex-col gap-y-8">
 	<h2 class="flex gap-2 text-4xl font-bold pb-1 border-b border-neutral"><StoreIcon />Stores</h2>
+	<div role="alert" class="alert bg-base-100 text-neutral-content">
+		<InfoIcon />
+		<span>
+			Upgrade to a <a href="/" class="link link-secondary">Store Account</a> for free (forever) and benefit
+			from an elevated experience filled with premium features tailored for professionals.
+		</span>
+	</div>
 	<div class="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-8">
 		{#each [1, 2, 3] as listing}
 			<StoreCard />
 		{/each}
-	</div>
-	<div role="alert" class="alert bg-base-100">
-		<InfoIcon />
-		<span>
-			Upgrade to a <a href="/" class="link link-primary">Store Account</a> for free (forever) and benefit
-			from an elevated experience filled with premium features tailored for professionals.
-		</span>
 	</div>
 </div>
