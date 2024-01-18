713a45b392a17baff180e9af25b2558a367e834e
Rabii Bouhestine <rabiibouhestine@outlook.com>
2024-01-18 14:36:29 +0100

    cleanup

--- src/routes/(app)/user/[id]/+page.svelte
+++ src/routes/(app)/user/[id]/+page.svelte
@@ -1,6 +1,8 @@
 <script>
 	import ListingCard from '$lib/components/ListingCard.svelte';
 
+	import SortIcon from '~icons/mdi/sort';
+	import ListingsIcon from '~icons/mdi/tag-multiple';
 	import Facebook from '~icons/mdi/facebook';
 	import Phone from '~icons/mdi/phone';
 	import Cog from '~icons/mdi/cog';
@@ -35,7 +37,20 @@
 			</a>
 		</div>
 	</div>
-	<div class="divider divider-start text-4xl font-bold">Listings</div>
+	<div
+		class="w-full flex gap-4 flex-col sm:flex-row justify-between sm:items-end pb-4 border-b border-neutral"
+	>
+		<div>
+			<h2 class="flex gap-2 text-4xl font-bold"><ListingsIcon />Listings</h2>
+			<p class="mt-2 text-neutral-content text-xs sm:text-sm">
+				{'state' + ', ' + 'city' + ', ' + 'platform' + ', ' + 'sellers' + ', ' + 'sort'}
+			</p>
+		</div>
+		<button class="btn btn-outline rounded-full">
+			<SortIcon />
+			Sort & Filter
+		</button>
+	</div>
 	<div class="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6 gap-6">
 		{#each data.listings as listing}
 			<ListingCard
