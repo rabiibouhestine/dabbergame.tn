6c5c04126b0458e4ae6917c14250fe931cfc330b
Rabii Bouhestine <rabiibouhestine@outlook.com>
2024-01-19 11:45:37 +0100

    ui

--- src/routes/(app)/listing/[id]/+page.svelte
+++ src/routes/(app)/listing/[id]/+page.svelte
@@ -13,7 +13,7 @@
 </script>
 
 <div class="flex items-center md:items-start flex-col md:flex-row gap-10">
-	<div class="min-w-52 flex flex-col justify-center items-center gap-4">
+	<div class="w-52 min-w-52 flex flex-col justify-center items-center gap-4">
 		<div class="flex flex-col gap-3">
 			<a href="/listings" class="transition ease-in-out hover:scale-105">
 				<GameCover
@@ -22,21 +22,47 @@
 					coverImageId={listing.game_cover}
 				/>
 			</a>
-			<a href="/user/{listing.profiles.id}" class="transition ease-in-out hover:scale-105">
+			<a href="/listings" class="transition ease-in-out hover:scale-105">
+				<div class="bg-base-100 rounded-xl overflow-hidden">
+					<div
+						class="w-full h-[80px] relative bg-gradient-to-tr from-accent via-primary to-secondary"
+					>
+						<!-- <img
+							src="https://vojislavd.com/ta-template-demo/assets/img/profile-background.jpg"
+							alt="profile"
+							class="w-full h-full object-cover"
+						/> -->
+					</div>
+					<div class="flex flex-col gap-2 items-center pb-4 -mt-8">
+						<div class="avatar placeholder">
+							<div
+								class="bg-neutral text-neutral-content rounded-full w-16 outline outline-8 outline-base-100"
+							>
+								<span class="text-3xl font-bold">{'firstName'[0] + 'lastName'[0]}</span>
+							</div>
+						</div>
+						<div class="text-center">
+							<h2 class="text-xl font-bold">{'Rabii' + ' ' + 'Bouhestine'}</h2>
+							<p class="text-sm text-slate-300">{'Ben Arous' + ', ' + 'El Mourouj'}</p>
+						</div>
+					</div>
+				</div>
+			</a>
+			<!-- <a href="/user/{listing.profiles.id}" class="transition ease-in-out hover:scale-105">
 				<UserInfo
 					firstName={listing.profiles.first_name}
 					lastName={listing.profiles.last_name}
 					state={listing.profiles.cities.state}
 					city={listing.profiles.cities.city}
 				/>
-			</a>
+			</a> -->
 		</div>
 		<div class="w-full flex flex-col gap-3">
 			<span class="btn btn-outline btn-secondary rounded-full text-lg font-bold"> Phone </span>
 			<span class="btn btn-outline btn-primary rounded-full text-lg font-bold"> Buy Now </span>
 		</div>
 	</div>
-	<div class="flex flex-col gap-6">
+	<div class="flex flex-col gap-6 flex-grow">
 		<div class="rounded-container flex flex-col justify-between gap-6">
 			<h2 class="text-4xl font-bold">{listing.game_name}</h2>
 			<div class="flex flex-wrap gap-3 justify-between">
--- src/routes/(app)/stores/+page.svelte
+++ src/routes/(app)/stores/+page.svelte
@@ -14,9 +14,32 @@
 			from an elevated experience filled with premium features tailored for professionals.
 		</span>
 	</div>
-	<div class="grid grid-cols-1 md:grid-cols-2 gap-8">
+	<div class="grid grid-cols-1 md:grid-cols-4 gap-8">
 		{#each [1, 2, 3, 4] as listing}
-			<StoreCard />
+			<div class="bg-base-100 rounded-xl overflow-hidden">
+				<div
+					class="w-full h-[80px] relative bg-gradient-to-tr from-accent via-primary to-secondary"
+				>
+					<!-- <img
+					src="https://vojislavd.com/ta-template-demo/assets/img/profile-background.jpg"
+					alt="profile"
+					class="w-full h-full object-cover"
+				/> -->
+				</div>
+				<div class="flex flex-col gap-2 items-center pb-4 -mt-8">
+					<div class="avatar placeholder">
+						<div
+							class="bg-neutral text-neutral-content rounded-full w-16 outline outline-8 outline-base-100"
+						>
+							<span class="text-3xl font-bold">{'firstName'[0] + 'lastName'[0]}</span>
+						</div>
+					</div>
+					<div class="text-center">
+						<h2 class="text-xl font-bold">{'Rabii' + ' ' + 'Bouhestine'}</h2>
+						<p class="text-sm text-slate-300">{'Ben Arous' + ', ' + 'El Mourouj'}</p>
+					</div>
+				</div>
+			</div>
 		{/each}
 	</div>
 </div>
