effe7c106f5b15a21685081bea111891c97d6a12
Rabii Bouhestine <rabiibouhestine@outlook.com>
2024-01-19 12:10:57 +0100

    cleanup

--- src/lib/components/UserCard.svelte
+++ src/lib/components/UserCard.svelte
@@ -1,4 +1,6 @@
 <script>
+	import StoreIcon from '~icons/mdi/store';
+
 	export let firstName = 'John';
 	export let lastName = 'Doe';
 	export let state = 'State';
@@ -18,11 +20,26 @@
 		{/if}
 	</div>
 	<div class="flex flex-col gap-2 items-center pb-4 -mt-8">
-		<div class="avatar placeholder">
+		<div class="avatar placeholder" class:indicator={isStore}>
+			{#if isStore}
+				<span
+					class="indicator-item indicator-bottom indicator-center badge badge-xs badge-secondary font-bold text-base-100 py-2"
+				>
+					<StoreIcon /> Store
+				</span>
+			{/if}
 			<div
 				class="bg-neutral text-neutral-content rounded-full w-16 outline outline-8 outline-base-100"
 			>
-				<span class="text-3xl font-bold">{firstName[0] + lastName[0]}</span>
+				{#if isStore}
+					<img
+						src="https://vojislavd.com/ta-template-demo/assets/img/profile-background.jpg"
+						alt="profile"
+						class="w-full h-full object-cover"
+					/>
+				{:else}
+					<span class="text-3xl font-bold">{firstName[0] + lastName[0]}</span>
+				{/if}
 			</div>
 		</div>
 		<div class="text-center">
