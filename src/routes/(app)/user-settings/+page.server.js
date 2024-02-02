29b51c529439b37fe096384a82b6d3b499e6cee8
Rabii Bouhestine <rabiibouhestine@outlook.com>
2024-02-02 11:01:31 +0100

    cleanup

--- src/routes/(app)/user-settings/+page.server.js
+++ src/routes/(app)/user-settings/+page.server.js
@@ -1,6 +1,15 @@
 import { AuthApiError } from "@supabase/supabase-js";
 import { fail, redirect } from "@sveltejs/kit";
 
+export async function load(event) {
+    const supabase = event.locals.supabase;
+
+    const { data } = await supabase.from("cities").select();
+    return {
+      cities: data ?? [],
+    };
+}
+
 export const actions = {
 	logout: async (event) => {
 		const { request, url, locals: { supabase } } = event;
--- src/routes/(app)/user-settings/+page.svelte
+++ src/routes/(app)/user-settings/+page.svelte
@@ -1,3 +1,98 @@
-<form action="?/logout" method="POST">
-	<button type="submit" class="btn btn-primary">Logout</button>
-</form>
+<script>
+	export let form;
+	export let data;
+
+	const uniqueStates = [...new Set(data.cities.map((city) => city.state))];
+	let selectedState;
+</script>
+
+<div class="rounded-container max-w-4xl mx-auto">
+	<div class="divider"></div>
+	<form class="flex flex-col gap-4" method="POST" action="/register">
+		<div class="flex gap-3">
+			<label class="form-control w-full">
+				<div class="label">
+					<span class="label-text">First Name</span>
+				</div>
+				<input
+					name="first_name"
+					type="text"
+					placeholder="First Name"
+					class="input input-bordered w-full"
+				/>
+			</label>
+			<label class="form-control w-full">
+				<div class="label">
+					<span class="label-text">Last Name</span>
+				</div>
+				<input
+					name="last_name"
+					type="text"
+					placeholder="Last Name"
+					class="input input-bordered w-full"
+				/>
+			</label>
+		</div>
+		<div class="flex gap-3">
+			<label class="form-control w-full">
+				<div class="label">
+					<span class="label-text">State</span>
+				</div>
+				<select class="select select-bordered" bind:value={selectedState}>
+					{#each uniqueStates as state}
+						<option>{state}</option>
+					{/each}
+				</select>
+			</label>
+			<label class="form-control w-full">
+				<div class="label">
+					<span class="label-text">City</span>
+				</div>
+				<select class="select select-bordered" name="city_id">
+					{#each data.cities.filter((city) => city.state === selectedState) as city}
+						<option value={city.id}>{city.city}</option>
+					{/each}
+				</select>
+			</label>
+		</div>
+		<label class="form-control w-full">
+			<div class="label">
+				<span class="label-text">Phone</span>
+			</div>
+			<input name="phone" type="text" placeholder="Phone" class="input input-bordered w-full" />
+		</label>
+		{#if form?.error}<p class="error">Invalid credentials!</p>{/if}
+		<button type="submit" class="btn btn-neutral">Save</button>
+	</form>
+	<div class="divider"></div>
+	<form class="flex flex-col gap-4" method="POST" action="/register">
+		<label class="form-control w-full">
+			<div class="label">
+				<span class="label-text">Password</span>
+			</div>
+			<input
+				name="password"
+				type="password"
+				placeholder="Password"
+				class="input input-bordered w-full"
+			/>
+		</label>
+		<label class="form-control w-full">
+			<div class="label">
+				<span class="label-text">Confirm Password</span>
+			</div>
+			<input
+				name="confirm_password"
+				type="password"
+				placeholder="Password"
+				class="input input-bordered w-full"
+			/>
+		</label>
+		{#if form?.error}<p class="error">Invalid credentials!</p>{/if}
+		<button type="submit" class="btn btn-neutral">Signup</button>
+	</form>
+	<div class="divider"></div>
+	<form action="?/logout" method="POST">
+		<button type="submit" class="btn btn-primary">Logout</button>
+	</form>
+</div>
